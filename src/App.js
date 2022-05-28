a026a3ace4e6cda05534d155aaf4220886a96605
Chris Lovell <clovellbsc@gmail.com>
2022-05-28 19:22:26 +0100

    added ability to return the most popular language with one repository returned from github

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [username, setUsername] = useState("")
+  const [language, setLanguage] = useState("")
+
+  const handleChange = (event) => {
+    const user = event.target.value
+    setUsername(user)
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const repositories = await axios.get(`https://api.github.com/users/${username}/repos?sort=updated&direction=desc`)
+    const language = repositories.data[0].language
+    setLanguage(language)
+  }
+
   return (
     <div>
       <h2>Search by github username to find their favourite language</h2>
       <form>
-        <input placeholder="Username" />
-        <button type="submit">Submit</button>
+        <input type="text" onChange={handleChange} value={username} placeholder="Username" />
+        <button type="submit" onClick={handleSubmit}>Submit</button>
       </form>
+      <h4>{language}</h4>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event'
+import mockAxios from 'axios'
+
+jest.mock("axios")
 
 test('renders learn react link', () => {
   render(<App />);
@@ -10,4 +14,43 @@ test('renders learn react link', () => {
   const submitButton = screen.getByRole("button", { name: "Submit" })
 
   expect(submitButton).toBeInTheDocument()
+});
+
+test('returns JavaScript when one Javascript repository is returned', async () => {
+  const mockResponse = { data: [ {language: "JavaScript"} ] }
+
+  mockAxios.get.mockResolvedValueOnce(mockResponse)
+
+  render(<App />);
+  const inputUsername = screen.getByPlaceholderText("Username")
+  const submitButton = screen.getByRole("button", { name: "Submit" })
+
+  userEvent.type(inputUsername, "clovellbsc")
+  userEvent.click(submitButton)
+
+  const language = await screen.findByText("JavaScript")
+
+  expect(language).toBeInTheDocument()
+});
+
+test('returns Ruby when one Ruby repository is returned', async () => {
+  const mockResponse = { data: [ {language: "Ruby"} ] }
+
+  mockAxios.get.mockResolvedValueOnce(mockResponse)
+
+  render(<App />);
+  const inputUsername = screen.getByPlaceholderText("Username")
+
+  expect(inputUsername).toBeInTheDocument()
+
+  const submitButton = screen.getByRole("button", { name: "Submit" })
+
+  expect(submitButton).toBeInTheDocument()
+
+  userEvent.type(inputUsername, "clovellbsc")
+  userEvent.click(submitButton)
+
+  const language = await screen.findByText("Ruby")
+
+  expect(language).toBeInTheDocument()
 });
