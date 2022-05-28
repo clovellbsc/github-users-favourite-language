d75d4c926ecc369103658b542bbcd7408c5bc765
Chris Lovell <clovellbsc@gmail.com>
2022-05-28 18:26:22 +0100

    form present to input username and to submit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <h2>Search by github username to find their favourite language</h2>
+      <form>
+        <input placeholder="Username" />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,13 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
+  const inputUsername = screen.getByPlaceholderText("Username")
+
+  expect(inputUsername).toBeInTheDocument()
+
+  const submitButton = screen.getByRole("button", { name: "Submit" })
+
+  expect(submitButton).toBeInTheDocument()
 });
